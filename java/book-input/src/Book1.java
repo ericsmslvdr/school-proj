59b4f5953bcdbc59df615b1fccc6291d8fe5e59e
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-12 18:15:26 +0800

    Update

---
+++ java/book-input/src/Book1.java
@@ -0,0 +1,31 @@
+
+public class Book1 {
+
+    private String title;
+    private String author;
+    private int totalPages;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTotalPages(int totalpgs) {
+        this.totalPages = totalpgs;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getTotalPages() {
+        return this.totalPages;
+    }
+}
---
+++ java/book-input/src/BookInputInfo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BookInputInfo extends Book1 {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        Book1 bkInput = new Book1();
+        
+        System.out.print("Enter the book title: ");
+        String title = input.nextLine();
+        bkInput.setTitle(title);
+        
+        System.out.print("Enter the book's author: ");
+        String author = input.nextLine();
+        bkInput.setAuthor(author);
+        
+        System.out.print("Enter the total number of pages: ");
+        int num = input.nextInt();
+        bkInput.setTotalPages(num);
+
+        System.out.println("\nThe book title is:  " + bkInput.getTitle());
+        System.out.println("The book's author is: " + bkInput.getAuthor());
+        System.out.println("The total number of pages: " + bkInput.getTotalPages());
+    }
+}
